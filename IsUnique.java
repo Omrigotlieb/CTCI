bd60957938efc681e54ee2540247874ce82ef8d4
Omri Gotlieb <omrigotlieb@10.142.55.39>
2017-03-26 11:13:04 -0700

    IsUnique Test

--- IsUnique.java
+++ IsUnique.java
@@ -1,27 +1,19 @@
 import java.util.Hashtable;
 
-/**
- * 
- */
-
-/**
- * @author omrigotlieb
- *
- */
 public class IsUnique {
 
     /**
      * @param args
      */
     public static void main(String[] args) {
-	System.out.println(isUnique("abcdefg"));
+	System.out.println(isUnique("aa"));
 
     }
     
     public static boolean isUnique(String str){
 	Hashtable hs = new Hashtable();
 	for (int i = 0; i < str.length(); i++) {
-	    if(null != hs.put(str.charAt(i),i)){
+	    if(null != hs.put(charAt(i),str.charAt(i))){
 		return false;
 	    }
 	}
